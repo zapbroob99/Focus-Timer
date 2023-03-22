52deba8b20ee93fa5fb11e839ab3179e23e90c75
enesAdisen <enes@car4future.link>
2023-03-22 23:35:26 +0300

    created initial interface. added app logo and a set goal button.

--- app/src/main/java/com/example/focustimer/MainActivity.java
+++ app/src/main/java/com/example/focustimer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.focustimer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button setGoalButton = findViewById(R.id.id_setGoal);
+        TextView testText = findViewById(R.id.id_test);
+        setGoalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setGoalButton.setVisibility(View.GONE);
+                testText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
