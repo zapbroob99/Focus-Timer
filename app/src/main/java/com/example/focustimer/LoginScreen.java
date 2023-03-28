299caa5a28e7f07b7f79acea2f1c289213ebc138
zapbroob99 <zapbroob@gmail.com>
2023-03-28 19:21:02 +0300

    changed colors, added a simple toolbar

--- app/src/main/java/com/example/focustimer/LoginScreen.java
+++ app/src/main/java/com/example/focustimer/LoginScreen.java
@@ -2,7 +2,10 @@ package com.example.focustimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginScreen extends AppCompatActivity {
@@ -13,6 +16,14 @@ public class LoginScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
+        Button btn = findViewById(R.id.btn_go);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
 
 
 
